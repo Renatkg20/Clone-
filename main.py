ee61e5d5c55b222516660e7f18dd3184d283db87
Rinat Galinurov <>
2021-04-29 04:55:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# wget 
+#     -m -l 10 -e robots=off -p -k -E --reject-regex "wp" --no-check-certificate 
+#     -U="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36" 
+#     site-addr.com
